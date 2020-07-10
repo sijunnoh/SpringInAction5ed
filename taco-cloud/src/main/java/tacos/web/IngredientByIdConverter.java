44c0b1ed7bc2f14f95541c5fddbf0625ca5ed9ec
sijunnoh <sijun.noh@gmail.com>
2020-07-10 23:27:22 +0900

    ADD ~page115

--- taco-cloud/src/main/java/tacos/web/IngredientByIdConverter.java
+++ taco-cloud/src/main/java/tacos/web/IngredientByIdConverter.java
@@ -1,5 +1,7 @@
 package tacos.web;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
@@ -10,14 +12,16 @@ import tacos.data.IngredientRepository;
 @Component
 public class IngredientByIdConverter implements Converter<String, Ingredient>{
 	private IngredientRepository ingredientRepo;
-	
+
 	@Autowired
 	public IngredientByIdConverter(IngredientRepository ingredientRepo) {
 		this.ingredientRepo = ingredientRepo;
 	}
-	
+
 	@Override
 	public Ingredient convert(String id) {
-		return ingredientRepo.findById(id);
+		Optional<Ingredient> optionalIngredient = ingredientRepo.findById(id);
+		return optionalIngredient.isPresent() ?
+				optionalIngredient.get() : null;
 	}
 }
