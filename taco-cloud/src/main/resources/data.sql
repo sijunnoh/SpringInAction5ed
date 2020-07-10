c733474523b69e16aa26b780fde58a58b06c3d09
sijunnoh <sijun.noh@gmail.com>
2020-07-10 22:05:03 +0900

    MODIFY data.sql

--- taco-cloud/src/main/resources/data.sql
+++ taco-cloud/src/main/resources/data.sql
@@ -5,7 +5,7 @@ delete from Taco_Order;
 
 delete from Ingredient;
 insert into Ingredient (id, name, type)
-    values ('FLTO', 'Flour Tortilla 토르티아', 'WRAP');
+    values ('FLTO', 'Flour Tortilla', 'WRAP');
 insert into Ingredient (id, name, type)
     values ('COTO', 'Corn Tortilla', 'WRAP');
 insert into Ingredient (id, name, type)
